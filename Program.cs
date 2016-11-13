442c9e76d0f319725c44d02b48129c8f7c88977f
Thomas Kim <Thomas@Thomass-MacBook-Pro.local>
2016-11-13 10:47:03 -0500

    first commit

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+
+namespace ConsoleApplication
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            /*
+                two pointers, one at head, one at end.
+                keep moving end pointer to head until not 0. then move head pointer
+                and keep swapping with end pointer
+            */
+
+        }
+
+        public static void ShiftZeros(int[] numbers) {
+
+            int head=0;
+            int tail = numbers.Length - 1;
+
+            while (tail > 0 && numbers[tail] ==0 )
+                tail--;
+            
+            while (head < tail) {
+                if (numbers[head] == 0)
+                {
+                    int temp = numbers[tail];
+                    numbers[tail] = numbers[head];
+                    numbers[head] = temp;
+                    tail--;
+                    
+                }
+                head++;
+            }
+        }
+
+        public List<int> BigMerge(int[][] listOfSorted) {
+
+            return null;
+        }
+    }
+}
